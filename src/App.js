bdca2156a38cd47301fa71fe90b45643d6d1d084
Personsk <112571152+Personsk@users.noreply.github.com>
2024-03-23 16:15:52 -0400

    werking

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Homepage from './Components/Homepage.jsx'; // Import the Homepage component
+import Header from './Components/Header.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Homepage />
     </div>
   );
