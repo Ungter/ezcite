6cb65171765146da6dcaf8aa7420c0c335f1006e
Personsk <112571152+Personsk@users.noreply.github.com>
2024-03-23 17:14:26 -0400

    still tryna fix image issue bs

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import ezCiteLogo from '../assets/svg/ezcite.svg'; // Import the SVG file
 import '../Templates/header.css';
+import { ReactComponent as EzCiteLogo } from '../assets/svg/ezcite.svg';
+
 
 const Header = () => {
   return (
     <header className="header">
-      <img src={require('../ezcite.png')} alt="Logo" style={{width:'200px', height:'60px'}} />
+      <div className="logo-container">
+        <img src={ezCiteLogo} alt="Logo" className="logo"/>
+        <div className="introduction">
+          <h1>Welcome to EzCite!</h1>
+          <p>The number 1 place to get citations for your paper.</p>
+        </div>
+      </div>
     </header>
   );
 };
