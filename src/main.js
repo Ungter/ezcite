24ad757fe07596559c384611ac8a18a0f637a2b3
Jungter <59937326+Ungter@users.noreply.github.com>
2024-03-24 02:54:14 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 // --------------- Imports --------------- 
 const axios = require('axios')
-const google = require('googlethis')
-const fs = require('fs')
+const { OpenAI } = require('openai')
+const openaiAPI = ""
+const openai = new OpenAI({
+    apiKey: openaiAPI
+})
+const perplexity = new OpenAI({
+    apiKey: openaiAPI,
+    base_url: "https://api.perplexity.ai"
+})
 
 // --------------- User input varibles ---------------
 
@@ -9,110 +16,53 @@ let inputPrompt = "Roe v. Wade, 410 U.S. 113 (1973),[1] was a landmark decision 
 let blacklistedWebsites;
 let citeStyle;
 
-// --------------- System varibles ---------------
-
-const claudeAPIKey = ""
-let keyIdeasFromPrompt;
-let sourcesFromMemory;
-let sourcesFromWeb;
-
 // --------------- Non-AI Helper Functions ---------------
 
 function setBlacklistedWebsites() {
 
 }
 
-async function googleSearch(query) {
-    const options = {
-        page: 0,
-        safe: false,
-        parse_ads: false,
-        additional_params: {
-            hl: 'en',
-            num: 10
-        }
-    }
+async function queryCiteLinks(query) {
 
-    // Create an array to hold all the promises
-    const promises = query.map(item => google.search(item, options));
+}
 
-    // Wait for all promises to resolve
-    const results = await Promise.all(promises);
-
-    // Write the results to a JSON file
-    fs.writeFile('results.json', JSON.stringify(results, null, 2), (err) => {
-        if (err) throw err;
-        console.log('Data written to file');
-    });
+async function helperGetLinks() {
+    setLinksSourcesByAI().then((result) => {
+        console.log(result)
+        return result
+    })
 }
 
 // --------------- AI Functions ---------------
 
-async function callAI(model, system, prompt ) {
-
-    let selectedModel;
-
-    if (prompt === undefined) {
-        return null;
-    }
-
-    switch (model) {
-        case "sonnet":
-            selectedModel = "claude-3-sonnet-20240229";
-            break;
-        case "haiku":
-            selectedModel = "claude-3-haiku-20240307";
-            break;
-        case "opus":
-            selectedModel = "claude-3-opus-20240229";
-            break;
-        default:
-            selectedModel = "claude-3-haiku-20240307";
-            break;
-    } 
-
-    let response = await axios.post('https://api.anthropic.com/v1/messages', {
-        model: selectedModel,
-        max_tokens: 1024,
-        system: system,
-        messages: [ 
-            {role: "user", content: prompt},
-            {role: "assistant", content: "{"}
-        ]
-    }, {
-        headers: {
-            "x-api-key": claudeAPIKey,
-            "anthropic-version": "2023-06-01",
-            "content-type": "application/json"
-        }
-    }) 
-
-    return response.data.content[0].text
-}
-
 async function getMainPromptIdeas() {
-    const systemPrompt = "Identify and summarize key short sentences, start with the first key idea and do not include anything else. Use JSON output format"
-    return await callAI("haiku", systemPrompt, inputPrompt)
+    const systemPrompt = "Identify a single phrase main topic, and summarize key short sentences, each short sentence should start with the single phrase topic. Use JSON output format"
+    const response = await openai.chat.completions.create({
+        model: "gpt-4-0125-preview",
+        response_format: { type: "json_object" },
+        seed: 1,
+        messages: [
+            {role: "system", content: systemPrompt},
+            {role: "user", content: inputPrompt}
+        ]
+    })
+    console.log(response.choices[0].message.content)
+    return response.choices[0].message.content
 }
 
-async function JSONhelper() {
-    let response = await getMainPromptIdeas();
+async function setLinksSourcesByAI() {
+    let textToCite = await getMainPromptIdeas()
+    const systemPrompt = `Identify possible creditble sources from the text, output in links. Use JSON output format.`
 
-    try {
-        JSON.parse(response);
-        return response;
-    } catch (error) {
-        const systemPrompt = "Convert the input into a valid JSON format. Use JSON output format."
-        return await callAI("sonnet", systemPrompt, response)
-    }
+    const response = await perplexity.chat.completions.create({
+        model: "gpt-4-0125-preview",
+        response_format: { type: "json_object" },
+        seed: 1,
+        messages: [
+            {role: "system", content: systemPrompt},
+            {role: "user", content: textToCite}
+        ]
+    })
+
+    return response.choices[0].message.content
 }
-
-async function determineSources() {
-    let textToCite = await JSONhelper()
-    const systemPrompt = `Identify possible sources from the text and cite them in MLA format. If not certain, do NOT cite them and output "N/A". Use JSON output format and NOTHING ELSE.`
-    return await callAI("opus", systemPrompt, textToCite)
-}
-
-determineSources().then((result) => {
-    console.log(result)
-})
